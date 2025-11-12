4644614b658f472c985c0a6979a1c314f3de7f2e
Adala Abu Middain <adalamiddain20@gmail.com>
2025-11-12 15:27:19 +0200

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -7,8 +7,47 @@
 #include <iostream>
 using namespace std;
 
+void Animal::display() {
+    cout<<"animal name:"<<name<<endl;
+    cout<<"animal age:"<<age<<endl;
+    cout<<"is hungry:"<<isHungry<<endl;
+}
+void Animal::feed() {
 
-// ============== MAIN FUNCTION ==============
+}
+void enclosure::addAnimal(Animal *animal) {
+    Animal* newanimal=new Animal[currentcount+1];
+    for (int i=0;i<currentcount;i++) {
+        newanimal[i]=animal[i];
+        newanimal[newanimal]=animal;
+        delete [] animal;
+        newanimal=animal;
+    }
+    void enclosure::displayanimal(){
+        cout<< getName<<endl;
+        cout<<getAge<<endl;
+    }
+    void Animal::Animal(string name, int age, bool ishungry) {
+        this->name=name;
+        this->age=age;
+        this->ishungry=ishungry;
+    }
+    void mammal(string name, int age, bool ishungry,string furcolor):Animal(name,age,ishungry){
+        this->furcolor=furcolor;
+    }
+    void bird(string name, int age, bool ishungry,float wingspan):Animal(name,age,ishungry) {
+        this-> wingspan= wingspan;
+    }
+    void reptile(string name, int age, bool ishungry, bool isvenomous):Animal(name,age,ishungry) {
+        this->isvenomous= isvenomous;
+    }
+    void displayinfo() {
+        cout<<vistorname<<endl;
+        cout<<ticketbought<<endl;
+
+    }
+};
+
 int main() {
     
     
